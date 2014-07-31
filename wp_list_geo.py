a0d22ad77c77ef1d91f89ef7913c0dc16f9b0ce9
siznax <steve@siznax.net>
2014-07-31 15:18:04 -0700

    added wp_list_geo.py, wplist.json

---
+++ wp_list_geo.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2.7
+
+__author__ = "@siznax"
+__version__ = "31 Jul 2014"
+
+import argparse
+import json
+import logging
+import lxml.html
+import os
+import requests
+import sys
+
+USER_AGENT = "GF&S"
+BASE_HREF = 'http://wikipedia.org'
+
+
+class WPListGeoData:
+  """crawl a Wikipedia list, fetching parsing children for geo data"""
+
+  def __init__(self, debug=False):
+    lgr = logging.getLogger(USER_AGENT)
+    if debug:
+      lgr.setLevel(logging.DEBUG)
+    else:
+      lgr.setLevel(logging.ERROR)
+    fmt = logging.Formatter('%(levelname)s: %(message)s')
+    clog = logging.StreamHandler(sys.stdout)
+    clog.setLevel(logging.DEBUG)
+    clog.setFormatter(fmt)
+    lgr.addHandler(clog)
+    self.log = lgr
+    self.data = []
+
+  def fetch(self, url, _file="seed.html"):
+    """write HTML to file from URL if not extant"""
+    if not os.path.exists(_file):
+      self.log.info("fetching %s" % url)
+      r = requests.get(url)
+      with open(_file, 'w') as fp:
+        fp.write(r.text.encode('utf-8'))
+        self.log.info("wrote %d bytes to %s" % (fp.tell(), _file))
+  
+  def parse(self, _file="seed.html"):
+    """parse lxml document from file"""
+    self.log.info("parsing %s" % _file)
+    with open(_file, 'r') as fp:
+      return lxml.html.fromstring(fp.read())
+  
+  def select(self, doc, selector):
+    """CSS select elements from documnet"""
+    self.log.info("selecting %s" % selector)
+    return doc.cssselect(selector)
+  
+  def hop(self, ind, elm):
+    """fetch and parse one hop away"""
+    dat = {}
+    dat["text"] = elm.text
+  
+    href = "%s/%s" % (BASE_HREF, elm.attrib['href'])
+    dat["href"] = href
+  
+    html = "{0:03d}.html".format(ind)
+    self.fetch(href, html)
+    doc = self.parse(html)
+    selectors = {"lat": "#coordinates .latitude",
+                 "lon": "#coordinates .longitude",
+                 "geo": "#coordinates .geo"}
+  
+    for key, sel in selectors.iteritems():
+      try:
+        dat[key] = self.select(doc, sel)[0].text
+      except:
+        dat[key] = None
+  
+    return dat
+
+
+def main(args):
+  lgd = WPListGeoData(args.debug)
+  lgd.fetch(args.seed)
+  for ind, elm in enumerate(lgd.select(lgd.parse(), args.CSS)):
+    # if ind >= 3: break
+    lgd.data.append(lgd.hop(ind, elm))
+  print json.dumps(lgd.data, indent=2, sort_keys=True)
+
+
+if __name__ == "__main__":
+  epi = ("e.g.", 
+         'http://en.wikipedia.org/wiki/List_of_museums_in_London',
+         "'.wikitable tr th a'")
+  argp = argparse.ArgumentParser(epilog=" ".join(epi))
+  argp.add_argument("-debug", action='store_true', default=False, 
+                    help="debug flag")
+  argp.add_argument("seed", help="Wikipedia list URL")
+  argp.add_argument("CSS", help="CSS selector")
+
+  main(argp.parse_args())
