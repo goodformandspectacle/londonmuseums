4c6700be6b607ff773a0cfc83a385f3ca8b76d12
siznax <steve@siznax.net>
2015-01-26 13:43:31 -0800

    added --geojson option.

--- expand_osm_ways.py
+++ expand_osm_ways.py
@@ -5,12 +5,15 @@ __author__ = "siznax"
 __date__ = "Jan 2015"
 
 import argparse
+import json
 
 from lxml import etree
 
 COMPACT = []
+GEOJSON = []
 FULL = []
 OSM = 'http://www.openstreetmap.org'
+
 
 def get_relation(root, way_id):
     """returns relation id from way id, if applicable."""
@@ -20,11 +23,13 @@ def get_relation(root, way_id):
         return member[0].getparent().get("id")
     return "relation"
 
+
 def get_way_name(way_elem):
     """returns name tag value from way element, if present."""
     name_tag = way_elem.xpath(".//tag[@k='name']")
     if len(name_tag):
         return name_tag[0].get("v")
+
 
 def walk_way_items(root, way_id):
     """prints details for each way path."""
@@ -33,16 +38,22 @@ def walk_way_items(root, way_id):
     name = get_way_name(way)
     if not name:
         name = "(%s/relation/%s)" % (OSM, relation)
-    COMPACT.append("%s/way/%s %s" % (OSM, way_id, name))
-    FULL.append("-- %s/way/%s %s" % (OSM, way_id, name))
+    heading = "%s/way/%s %s" % (OSM, way_id, name)
+    COMPACT.append(heading)
+    FULL.append("-- %s" % heading)
+    path = []
     for item in way:
         if item.tag == 'nd':
             nd_ref = item.get("ref")
             node = root.xpath("//node[@id='%s']" % nd_ref)[0]
-            lat = node.get("lat")
-            lon = node.get("lon")
-            row = " ".join([relation, way_id, nd_ref, lat, lon])
+            lat = float(node.get("lat"))
+            lon = float(node.get("lon"))
+            row = " ".join([relation, way_id, nd_ref, str(lat), str(lon)])
+            path.append([lon, lat])
             FULL.append(row)
+    GEOJSON.append({'way': heading,
+                    'geo': {'type': 'LineString',
+                            'coordinates': path}})
 
 
 def walk_ways(root):
@@ -57,6 +68,9 @@ def main(_file, args):
     if args.compact:
         print "\n".join(COMPACT)
         return
+    if args.geojson:
+        print json.dumps(GEOJSON, indent=4, separators=(',', ': '))
+        return
     print "rel way node lat lon"
     print "\n".join(FULL)
 
@@ -64,6 +78,8 @@ def main(_file, args):
 if __name__ == "__main__":
     argp = argparse.ArgumentParser(description="Expand OSM ways.")
     argp.add_argument("-c", "--compact", action="store_true",
-                      help="show compact list")
+                      help="emit compact list")
+    argp.add_argument("-g", "--geojson", action="store_true",
+                      help="emit GEOJSON list")
     args = argp.parse_args()
     main("overpass.xml", args)
