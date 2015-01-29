60c5a42d1568d2d777934187be943f920e75e3fa
siznax <steve@siznax.net>
2015-01-28 17:38:04 -0800

    added write_csv() to expand_osm_ways.py

--- expand_osm_ways.py
+++ expand_osm_ways.py
@@ -5,13 +5,15 @@ __author__ = "siznax"
 __date__ = "Jan 2015"
 
 import argparse
-import json
+import csv
 
 from lxml import etree
 
 OUTPUT = {}
+CSVFILENAME = 'ways.csv'
 OSM = 'http://www.openstreetmap.org'
 FIELDNAMES = ['size', 'label', 'relation', 'way', ]
+
 
 def get_relation(root, way_id):
     """returns relation id from way id, if applicable."""
@@ -33,7 +35,7 @@ def path_width(path):
     """returns greatest difference in longitude (east/west)"""
     lons = [abs(x[0]) for x in path]
     base = [lons[0]] * len(lons)
-    diff = [abs(i - j) for i, j in zip(lons,base)]
+    diff = [abs(i - j) for i, j in zip(lons, base)]
     return max(diff)
 
 
@@ -41,7 +43,7 @@ def path_height(path):
     """returns greatest difference in latitude (north/south)"""
     lats = [abs(x[1]) for x in path]
     base = [lats[0]] * len(lats)
-    diff = [abs(i - j) for i, j in zip(lats,base)]
+    diff = [abs(i - j) for i, j in zip(lats, base)]
     return max(diff)
 
 
@@ -72,10 +74,22 @@ def walk_way_items(root, way_id):
                       way_id)  # avoid collisions
     OUTPUT[size] = {'size': size, 'way': way_id, 'label': label,
                     'coords': coords, 'nodes': nodes,
-                    'geo': {'type': 'LineString','coordinates': path},
+                    'geo': {'type': 'LineString', 'coordinates': path},
                     'wlink': get_link('way', way_id),
                     'rlink': get_link('relation', rel_id)}
-    
+
+
+def write_csv(output):
+    fieldnames = ['size', 'way', 'label']
+    with open(CSVFILENAME, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in sorted(output):
+            _size = output[item]['size'].split('_')[0]
+            writer.writerow({'size': _size,
+                             'way': output[item]['wlink'],
+                             'label': output[item]['label']})
+        return csvfile.tell()
 
 
 def walk_ways(root):
@@ -86,10 +100,14 @@ def walk_ways(root):
 def main(_file, args):
     tree = etree.parse(_file)
     walk_ways(tree.getroot())
+    if args.writecsv:
+        nbytes = write_csv(OUTPUT)
+        print "wrote %s bytes to %s" % (nbytes, CSVFILENAME)
     for sizekey in sorted(OUTPUT):
         if args.format == "list":
-            print "%s %s" % (OUTPUT[sizekey]['size'].split('_')[0],
-                             OUTPUT[sizekey]['label'])
+            print "%s %s %s" % (OUTPUT[sizekey]['size'].split('_')[0],
+                                OUTPUT[sizekey]['wlink'],
+                                OUTPUT[sizekey]['label'])
         if args.format == "dump":
             print OUTPUT[sizekey]
         if args.format == "geojson":
@@ -97,7 +115,10 @@ def main(_file, args):
 
 
 if __name__ == "__main__":
-    argp = argparse.ArgumentParser(description="Expand OSM ways and emit by size.")
-    argp.add_argument("format", choices=['list','dump','geojson'])
+    argp = argparse.ArgumentParser(
+        description="Expand OSM ways and emit by size.")
+    argp.add_argument("format", choices=['list', 'dump', 'geojson'])
+    argp.add_argument('-c', '--writecsv', action="store_true",
+                      help="write CSV to %s" % CSVFILENAME)
     args = argp.parse_args()
     main("overpass.xml", args)
